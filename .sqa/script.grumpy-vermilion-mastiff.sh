2129a0bb9871333055f17e44f9007e242792dc3e
Pablo Orviz <orviz@ifca.unican.es>
2025-07-21 01:15:58 +0200

    Add JePL file structure

--- .sqa/script.grumpy-vermilion-mastiff.sh
+++ .sqa/script.grumpy-vermilion-mastiff.sh
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 (
-cd github.com/rciam/rciam-federation-registry &&
-    git rev-parse --is-inside-work-tree 
+cd github.com/rciam/rciam-federation-registry-docs &&
+    mdl --json  .
 )
